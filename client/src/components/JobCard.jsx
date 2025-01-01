6a3126ea109a128f7b128e8889b810303a5e9c5e
Om Sitapara <omsitapara5@gmail.com>
2025-01-01 19:11:44 +0530

    job info page

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const JobCard = ({job}) => {
+
+    const navigate= useNavigate()
+
   return (
-    <div className='border p-6 shadow rounded'>
+    <div className='border p-6 shadow rounded mt-4'>
         <div className='flex justify-between items-center'>
             <img className='h-8' src={assets.company_icon} />
         </div>
@@ -18,8 +22,8 @@ const JobCard = ({job}) => {
         </div>
         <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
         <div className='mt-4 flex gap-4 text-sm'>
-            <button className='bg-blue-600 text-white px-4 py-2 rounded'>Apply Now</button>
-            <button className='text-gray-500 border border-gray-500 rounded px-4 py-2'>Learn More</button>
+            <button onClick={()=> {navigate(`/apply-job/${job._id}`); scrollTo(0,0)}} className='bg-blue-600 text-white px-4 py-2 rounded'>Apply Now</button>
+            <button onClick={()=> {navigate(`/apply-job/${job._id}`); scrollTo(0,0)}} className='text-gray-500 border border-gray-500 rounded px-4 py-2'>Learn More</button>
         </div>
     </div>
   )
